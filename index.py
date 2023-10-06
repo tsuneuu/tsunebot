a52d2c3d16e26cd781c63ce1259fc14b500d7b88
Nikita Pankratov <foxerevonp@gmail.com>
2023-10-06 23:04:44 +1000

    New function in index.py

--- index.py
+++ index.py
@@ -1,1 +1,7 @@
-print("Main screen of bot")
+def my_foo(name: str) -> str:
+    return "Привет, {0}".format(name)
+
+name = input("Введите ваше имя: ")
+hello_message = my_foo(name)
+
+print(hello_message)
